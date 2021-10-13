aefab6e13b45ed5c59eecb3154e68ca38ed121ed
KyleRess-Liere <70179439+KyleRess-Liere@users.noreply.github.com>
2021-10-12 20:15:59 -0400

    Mermory sections forgot to clik on file

--- source/host/memory.ts
+++ source/host/memory.ts
@@ -1,7 +1,13 @@
 module TSOS {
 
     export class Memory {
-        public memoryArray: string[] = new Array(0x100);
+        public memoryArray: string[] = new Array(768);
+        private memorySectionOneBase = 0;
+        private memorySectionOneEnd = 255;
+        private memorySectionTwoBase = 256;
+        private memorySectionTwoEnd = 511;
+        private memorySectionThreeBase = 512;
+        private memorySectionThreeEnd = 767;
 
       
 
@@ -9,11 +15,41 @@ module TSOS {
         }
 
         public init(): void {
-            // initialize memeory
+            // initialize memory
             for (var i = 0; i < this.memoryArray.length; i++) {
                 this.memoryArray[i] = "00";
             }
         }
+        public getSectionBase(section:string){
+            switch(section){
+            case "1": 
+            return this.memorySectionOneBase
+            break;
+            case "2": 
+            return this.memorySectionTwoBase
+            case "3": 
+            return this.memorySectionThreeBase
+            default:
+                console.log("Invalid section")
+            }
+
+        }
+        public getSectionEnd(section:string){
+            switch(section){
+                case "1": 
+                return this.memorySectionOneEnd;
+                break;
+                case "2": 
+                return this.memorySectionTwoEnd;
+                break;
+                case "3": 
+                return this.memorySectionThreeEnd
+                break;
+                default:  
+                console.log("Invalid section number")
+            }
+        }
+       
 
     }
 }
