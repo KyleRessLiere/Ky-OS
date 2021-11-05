f513b2a73b7c63bf98e6d0e2fdaa65e4ab35514c
KyleRess-Liere <70179439+KyleRess-Liere@users.noreply.github.com>
2021-11-05 11:11:10 -0400

    Created scheduler

---
+++ distrib/os/scheduler.js
@@ -0,0 +1,14 @@
+/* ------------
+This will do the logic for the CPU scheduling when their are mutple running processes.
+*/
+var TSOS;
+(function (TSOS) {
+    class Scheduler {
+        constructor() { }
+        //determines which process will be running processe
+        currentProcess() {
+        }
+    }
+    TSOS.Scheduler = Scheduler;
+})(TSOS || (TSOS = {}));
+//# sourceMappingURL=scheduler.js.map
---
+++ source/os/scheduler.ts
@@ -0,0 +1,12 @@
+/* ------------
+This will do the logic for the CPU scheduling when their are mutple running processes.
+*/
+module TSOS {
+    export class Scheduler{
+        constructor() {}
+        //determines which process will be running processe
+        public currentProcess(){
+
+        }
+    }
+}
